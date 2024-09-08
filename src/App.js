5e6d6a2c80a6886eef5e176664925d9b24cd7ad1
rajsethi05 <rajsethi@Rajs-MacBook-Air.local>
2024-09-08 18:54:10 -0400

    added intro page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import IntroPage from './components/IntroPage';
 
 function App() {
-  return (<Navbar/>);
+  return (
+    <>
+    <Navbar />
+      <IntroPage />
+      </>
+  );
 }
 
 export default App;
