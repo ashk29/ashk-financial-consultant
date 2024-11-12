0997ff5ac683702a10bd943d843aad76adc8d696
rajsethi05 <rajsethi@Rajs-MacBook-Air.local>
2024-11-11 19:58:27 -0500

    Updates

---
+++ precache-manifest.0dc27db4b77c545f3bb53d4713ae20b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ece9c19d950b4c7d3e13091c436c224",
+    "url": "/ashk-financial-consultant/index.html"
+  },
+  {
+    "revision": "dbe2d76329ad0831dae4",
+    "url": "/ashk-financial-consultant/static/css/main.1f4b6639.chunk.css"
+  },
+  {
+    "revision": "44f312ce0e386ecacfd6",
+    "url": "/ashk-financial-consultant/static/js/2.d1323b0b.chunk.js"
+  },
+  {
+    "revision": "107bcb1859c73535a9fe2555a40ce86f",
+    "url": "/ashk-financial-consultant/static/js/2.d1323b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe2d76329ad0831dae4",
+    "url": "/ashk-financial-consultant/static/js/main.be523254.chunk.js"
+  },
+  {
+    "revision": "fe8c9044f0905af15101",
+    "url": "/ashk-financial-consultant/static/js/runtime-main.6388fa09.js"
+  }
+]);
